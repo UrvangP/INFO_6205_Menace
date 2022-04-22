b9650dff22aabb9b25641e510a05ac166af2345b
Prathamesh Nemade <nemade.p@northeastern.edu>
2022-04-21 22:24:56 -0400

    states generate. Pattern check pending

--- src/BaseState.java
+++ src/BaseState.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class BaseState {
     public BaseState() {
@@ -32,6 +33,8 @@ public class BaseState {
 
             if (question.equals(current)) {
                 strToCompare += "?";
+            } else {
+                strToCompare += "%";
             }
         }
 
@@ -39,9 +42,83 @@ public class BaseState {
         return this.allWinWinStates.containsKey(strToCompare);
     }
 
+
+    public void generateMoves() {
+        for (int i = 0; i < this.allMoves.length; i++) {
+            System.out.println(this.allMoves[i]);
+        }
+    }
+
+    public HashMap<String, Object> verify(ArrayList<String> state) {
+        int xs = 0;
+        int os = 0;
+        int i = 0;
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+
+        while (i < state.size()) {
+            String curr = state.get(i);
+            if (curr.equals("1")) xs++;
+            if (curr.equals("2")) os++;
+            if (curr.equals("0")) temp.add(i);
+            i++;
+        }
+
+        HashMap<String, Object> result = new HashMap<String, Object>();
+        result.put("data", temp);
+
+        if (xs == 0 && os == 0) {
+            result.put("val", false);
+            return result;
+        }
+
+
+        if (xs == os || xs == (os - 1) || os == (xs - 1)) {
+            result.put("val", true);
+            return result;
+        }
+
+        result.put("val", false);
+        return result;
+    }
+
+
+    public void compute(ArrayList<String> track) {
+        if (track.size() == this.validLettersTTT) {
+            HashMap<String, Object> data = this.verify(track);
+            if (data.get("val").equals(true)) {
+                String encoded = this.concatenateMe(track);
+                if (!isValidWinWinMove(encoded)) this.allCombinations.put(encoded, data.get("data"));
+            }
+            return;
+        }
+        for (int i = 0; i < this.allMoves.length; i++) {
+
+            track.add(this.allMoves[i]);
+            this.compute(track);
+            track.remove(track.size() - 1);
+
+        }
+    }
+
+    public String concatenateMe(ArrayList<String> arr) {
+        String str = new String();
+        for (int i = 0; i < arr.size(); i++) {
+            str += arr.get(i);
+        }
+        return str;
+    }
+
+    public HashMap<String, Object> getAllCombinations() {
+        return this.allCombinations;
+    }
+
     public HashMap<String, Boolean> getWinWinStates() {
         return this.allWinWinStates;
     }
 
     private HashMap<String, Boolean> allWinWinStates = new HashMap<String, Boolean>();
+    private String[] allMoves = new String[]{"0", "1", "2"};
+    private Integer validLettersTTT = 9;
+    private HashMap<String, Object> allCombinations = new HashMap<String, Object>();
+
 }
--- src/MenaceBegin.java
+++ src/MenaceBegin.java
@@ -5,11 +5,19 @@ public class MenaceBegin extends BaseState {
     public static void main(String[] args) {
         BaseState baseInstance = new BaseState();
 
-        Boolean isValid = baseInstance.isValidWinWinMove("111007000");
-        
+        Boolean isValid = baseInstance.isValidWinWinMove("120102100");
+
         System.out.println("isValid:- " + isValid);
-        for (Map.Entry<String, Boolean> entry : baseInstance.getWinWinStates().entrySet()) {
-            System.out.println(entry.getKey() + " = " + entry.getValue());
-        }
+//        baseInstance.generateMoves();
+//        baseInstance.compute(new ArrayList<String>());
+//        for (Map.Entry<String, Boolean> entry : baseInstance.getWinWinStates().entrySet()) {
+//            System.out.println(entry.getKey() + " = " + entry.getValue());
+//        }
+
+//        for (Map.Entry<String, Object> entry : baseInstance.getAllCombinations().entrySet()) {
+//            System.out.println(entry.getKey() + " = " + entry.getValue());
+//        }
+
+        System.out.println("total combinations: " + baseInstance.getAllCombinations().size());
     }
 }
