d3b81faf7eb38e0de186789d9866835e86278412
urvang96 <urvang96@gmail.com>
2022-04-26 10:09:15 -0400

    Added human test case

--- src/test/java/HumanTest.java
+++ src/test/java/HumanTest.java
@@ -124,5 +124,55 @@ public class HumanTest {
         assertTrue(human.fork());
         human = new Human( new int[][]{{0,0,2},{0,1,0},{2,0,1}} );
         assertTrue(human.fork());
+
+        human = new Human( new int[][]{{0,0,2},{0,1,0},{2,1,0}} );
+        assertFalse(human.fork());
+    }
+
+    @Test
+    public void testForkBlock(){
+        Human human = new Human( new int[][]{{1,0,0},{0,2,0},{0,0,1}} );
+        assertTrue(human.forkBlock());
+        human = new Human( new int[][]{{0,0,1},{0,2,0},{1,0,0}} );
+        assertTrue(human.forkBlock());
+
+        human = new Human( new int[][]{{0,0,1},{0,2,0},{0,1,0}} );
+        assertFalse(human.forkBlock());
+    }
+
+    @Test
+    public void testCenter(){
+        Human human = new Human( new int[][]{{0,0,1},{0,0,0},{0,0,0}} );
+        assertTrue(human.isCenterEmpty());
+
+        human = new Human( new int[][]{{0,0,0},{0,1,0},{0,0,0}} );
+        assertFalse(human.isCenterEmpty());
+    }
+
+    @Test
+    public void testSide(){
+        Human human = new Human( new int[][]{{0,0,1},{0,0,0},{0,0,0}} );
+        assertTrue(human.isSideEmpty());
+
+        human = new Human( new int[][]{{0,1,0},{2,0,2},{0,1,0}} );
+        assertFalse(human.isSideEmpty());
+    }
+
+    @Test
+    public void testCorner(){
+        Human human = new Human( new int[][]{{0,0,0},{0,1,0},{0,0,0}} );
+        assertTrue(human.isCornerEmpty());
+
+        human = new Human( new int[][]{{1,0,2},{0,0,0},{2,0,1}} );
+        assertFalse(human.isCornerEmpty());
+    }
+
+    @Test
+    public void testHumanStrat(){
+        Human human = new Human( new int[][]{{0,0,0},{0,1,0},{0,0,0}} );
+        assertTrue(human.humanStrat());
+
+        human = new Human( new int[][]{{1,2,1},{2,1,2},{1,2,1}} );
+        assertFalse(human.humanStrat());
     }
 }
